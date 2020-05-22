ed13ad09257bb572bc0957d3bc7c7a37ccc6ab44
Marco Gazzola <dev@marcogazzola.com>
2020-05-22 18:58:17 +0200

    code refactored managed log message managed case insensitive refactored master sensor attributes

--- apps/hassio-arpa-air-quality/arpaBaseClass.py
+++
@@ -1,7 +0,0 @@
-import appdaemon.plugins.hass.hassapi as hass
-
-class arpa_base_class(hass.Hass):
-
-	@abstractmethod
-	def getResult(self, url, station_id, arpa_monitored_params):
-		pass
--- apps/hassio-arpa-air-quality/arpa_air_quality.py
+++ apps/hassio-arpa-air-quality/arpa_air_quality.py
@@ -13,58 +13,55 @@ class arpa_air_quality_notify:
 class arpa_air_quality(hass.Hass):
 
 	def initialize(self):
-		self.log("########### ARPA AIR QUALITY INIT ###########")
+		self.log("########### ARPA AIR QUALITY INIT ###########", level = 'INFO')
 		try:
 			with open('{}/{}/region.json'.format(
 				self.app_dir, self.get_arg(self.args, 'app_folder_name'))) as json_file:
 				self.config = json.load(json_file)
-				self.log("self.config {}".format(self.config))
+				self.log("self.config {}".format(self.config), level = 'DEBUG')
 				self.regionsConfig = self.config["regions"]
-				self.log("self.regionsConfig {}".format(self.regionsConfig))
+				self.log("self.regionsConfig {}".format(self.regionsConfig), level = 'DEBUG')
 				self.timer_interval = int(self.config["refresh_rate"]) * 60 * 60
-				self.log("self.timer_interval {}".format(self.timer_interval))
+				self.log("self.timer_interval {}".format(self.timer_interval), level = 'DEBUG')
 		except Exception as inst:
 			self.regionsConfig = None
 			self.notify_frontend("ERROR", 
 				"Configurazione non presente.\nVerificare la presenza e il formato del file region.json",
 				arpa_air_quality_notify.configError)
-			self.log(inst)
+			self.log(inst, level = 'ERROR')
 			return
 
 		self.listen_event(self.arpa_air_quality_refresh, "arpa_air_quality_refresh")
 
 		self.throttle_timer = self.run_every(self.throttle_fetch_data, datetime.now() + timedelta(seconds=20), self.timer_interval)
 
-		self.log("########### ARPA AIR MONITOR END INIT ###########")
+		self.log("########### ARPA AIR MONITOR END INIT ###########", level = 'INFO')
 
 	def terminate(self):
 		self.cancel_timer(self.throttle_timer)
 
 	def throttle_fetch_data(self, kwargs):
-		self.log("throttle_fetch_data {}".format(kwargs), level = 'INFO')
+		self.log("throttle_fetch_data {}".format(kwargs), level = 'DEBUG')
 		self.fetch_data()
 
 	def arpa_air_quality_refresh(self, event_name, data, kwargs):
-		self.log("Event: {}".format(event_name))
-		self.log("Event data: {}".format(data))
+		self.log("Event: {}".format(event_name), level = 'DEBUG')
+		self.log("Event data: {}".format(data), level = 'DEBUG')
 		if "regioni" in data:
 			event_refresh_regions = dict((k, self.regionsConfig[k]) for k in data["regioni"] if k in self.regionsConfig)
-			self.log("1. {}".format(event_refresh_regions))
-			self.log("2. {}".format(event_refresh_regions.items()))
-			self.log("3. {}".format(list(event_refresh_regions)))
+			self.log("regions to refresh: {}".format(event_refresh_regions), level = 'INFO')
 			if event_refresh_regions:
 				#throttle function to ensure that we don't call check multiple times
 				self.cancel_timer(self.throttle_timer)
 				self.fetch_data(event_refresh_regions)
-				self.throttle_timer = self.run_every(self.throttle_fetch_data, datetime.now() + timedelta(seconds=10), self.timer_interval)
+				self.throttle_timer = self.run_every(self.throttle_fetch_data, datetime.now() + timedelta(seconds=self.timer_interval), self.timer_interval)
 
 	def fetch_data(self, regions = None):
-		self.log("fetch_data.1 {}".format(regions))
+		self.log("Fetch_data {}".format(regions), level = 'DEBUG')
 		regions = self.regionsConfig if regions is None else regions
-		self.log("fetch_data.2 {}".format(regions))
 
 		if not regions:
-			self.log("No regions configured", level = "WARNING")
+			self.log("Attention! No regions configured", level = "WARNING")
 			self.notify_frontend("WARNING", 
 				"Non ci sono regioni configurate. Verificare il file regions.json",
 				arpa_air_quality_notify.noDataFound)
@@ -82,22 +79,19 @@ class arpa_air_quality(hass.Hass):
 						arpa_air_quality_notify.appNotFound)
 					continue
 				regionApp.createSensor = self.config.get('create_sensor')
-				self.log("regionApp {}".format(regionApp))
-				self.log("1.unit_of_measurement {}".format(self.config.get('unit_of_measurement')))
+				self.log("regionApp {}".format(regionApp), level = 'DEBUG')
 				sensorAttributes = regionApp.getResult(regions.get(region).get('url'),
 					regions.get(region).get('station_id'),
 					regions.get(region).get('monitored_params'),
 					self.config.get('unit_of_measurement'))
-				self.log("sensorAttributes {}".format(sensorAttributes))
 				self.set_state("sensor.arpa_air_station_{}".format(region), 
-					state="Stazione di monitoraggio Arpa regione {} Stazione {}".format(
-						region.capitalize(), regions.get(region).get('station_id')), 
+					state="{}".format(regionApp.region_friendly_name), 
 					attributes = sensorAttributes)
 			except Exception as inst:
 				self.notify_frontend("ERROR", 
 					"Errore generico durante l'aggiornamento della regione {} - {}.".format(region, inst),
 					arpa_air_quality_notify.configError)
-				self.log("ERROR: {}".format(inst), level = "ERROR")
+				self.log("ERROR 2: {}".format(inst), level = "ERROR")
 				return
 
 	def get_arg(self, args, key):
